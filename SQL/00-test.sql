57b24d4815a37998f40f62999605bf2b09776d83
oklm33314 <oklm33314@gmail.com>
2025-06-30 17:43:54 +0900

    sql basic

---
+++ SQL/00-test.sql
@@ -0,0 +1,15 @@
+-- 00-test.sql
+
+-- ctrl + Enter
+
+select version();
+
+-- 게시판 DB
+-- 회원 정보 T
+-- 게시글 T
+-- 댓글 T
+
+-- 게임 DB
+-- 회원 정비 T
+
+-- 수업 DB
---
+++ SQL/01-createdb.sql
@@ -0,0 +1,16 @@
+-- 01-createdb.sql
+
+-- db 생성
+CREATE DATABASE sample_db;
+-- db 확인
+SHOW DATABASES; 
+-- db 삭제
+DROP DATABASE sample_db;
+
+-- 1. lecture, practice 생성 후 확인
+CREATE DATABASE lecture;
+CREATE DATABASE practice;
+SHOW DATABASES; 
+-- DB 사용 
+USE lecture;
+
